d93e00aa56e0050af4b393bc20eee057a7b4d74d
nazar110 <nazar1124@gmail.com>
2020-03-20 00:58:03 +0200

    Fixed some errors

---
+++ Kursova.DAL/EF/KursovaDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Kursova.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kursova.DAL.EF
+{
+    public class KursovaDbContext : DbContext
+    {
+        public DbSet<StudentEntity> Students { get; set; }
+        public DbSet<TeacherEntity> Teachers { get; set; }
+        public DbSet<TaskItemEntity> TaskItems { get; set; }
+        public DbSet<CommentEntity> Comments { get; set; }
+        public DbSet<DocumentationEntity> Documentations { get; set; }
+        public DbSet<TaskChangesEntity> TaskChanges { get; set; }
+        public DbSet<AdminEntity> Admins { get; set; }
+
+
+    }
+}
---
+++ Kursova.DAL/Entities/AdminEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kursova.DAL.Entities
+{
+    public class AdminEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
