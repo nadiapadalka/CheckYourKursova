e9c52daacf4074c0dacaee3669c8276273f07ae1
nadiapadalka <nadiapadalka21@gmail.com>
2020-03-22 22:35:06 +0200

    Changed structure of project

---
+++ CheckYourKursova/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kursova.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Group { get; set; }
+        public string Kafedra { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ CheckYourKursova/Models/TaskItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kursova.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Grade { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime DeadLine { get; set; }
+        public TimeSpan EstimatedTime { get; set; }
+        public bool IsDone { get; set; }
+        public List<TaskChanges> Changes { get; set; }
+
+
+    }
+}
