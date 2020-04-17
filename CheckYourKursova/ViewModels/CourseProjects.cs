3b35e65a1696e0b316c582c250c2feb34b2f085d
nazar110 <nazar1124@gmail.com>
2020-04-17 16:57:26 +0300

    Added some functionality

---
+++ CheckYourKursova/ViewModels/CourseProjects.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kursova.ViewModels
+{
+    public class CourseProjects
+    {
+        public List<ProjectInfo> AllProjects { get; set; } = new List<ProjectInfo>();
+
+        public ProjectInfo CurrentProject { get; set; } = new ProjectInfo();
+
+        public CourseProjects()
+        {
+            this.AllProjects.Add(
+            new ProjectInfo()
+            {
+                StudentName = "Shevchenko T. H.",
+                CourseProjectName = "Web Development using ASP.NET Core",
+                StudentMaterials = new List<string>()
+                {
+                        "Check Your Kursova.pdf",
+                        "testFileToCheckDownloading.xml",
+                        "TestFileToCheckDownloading.docx",
+                },
+            }
+            );
+            this.CurrentProject = this.AllProjects[0];
+        }
+    }
+}
